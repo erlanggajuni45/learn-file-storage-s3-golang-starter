2343c679b5116363428ccd33b51ce975a254d175
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-26 09:27:01 +0700

    load S3 client

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 
 	"github.com/joho/godotenv"
@@ -21,11 +24,7 @@ type apiConfig struct {
 	s3Region         string
 	s3CfDistribution string
 	port             string
-}
-
-type thumbnail struct {
-	data      []byte
-	mediaType string
+	s3Client         *s3.Client
 }
 
 func main() {
@@ -92,6 +91,12 @@ func main() {
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
 	}
+
+	configAws, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(cfg.s3Region))
+	if err != nil {
+		log.Fatalf("Couldn't load AWS config: %v", err)
+	}
+	cfg.s3Client = s3.NewFromConfig(configAws)
 
 	err = cfg.ensureAssetsDir()
 	if err != nil {
